c44bcd177dd1c74de97bc26ad73d53e16dab6239
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2017-01-20 12:43:18 -0500

    Starts with binary search, goes to linear when 1 egg remains.

--- drop.c
+++ drop.c
@@ -2,42 +2,62 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "egg.h"
-
-int bruteUp(egg *e)
+void checkAndPrint(egg *e, int floor)
 {
-    int count = 0;
+    if(egg_is_broken(e) != 0)
+    {
+        printf("#floor %d CRACK\n", floor);
+    }
+    else
+    {
+        printf("#floor %d safe\n", floor);
+    }
+}
+
+int bruteUp(egg *e, int bottom, int *drops)
+{
+    int count = bottom;
+    printf("BOTTOM: %d\n", bottom);
     printf("egg status: %d\n", egg_is_broken(e));
     while(egg_is_broken(e) == 0)
     {
         ++count;
+        ++*drops;
         egg_drop_from_floor(e, count);
+        checkAndPrint(e, count);
     }
+    printf("%d is the maximum safe floor, found after %d drops.\n", count - 1, *drops);
     return(count - 1);
 }
 
-int findFloor(egg **eggs, int *egg_count, int floors)
+int findFloor(egg *e, int *egg_count, long int floors, int *drops)
 {
-    int drops = 0;
+    //int drops = 0;
     int top = floors;
     int bottom = 0;
-    printf("count: %d\n", *egg_count);
     int half = floors / 2;
-    while(*egg_count >= 1)
+    while((*egg_count > 0) || (top == bottom + 1))
     {
         if(top == bottom + 1)
         {
-            printf("%d is the maximum safe floor, found after %d drops.\n", half - 1, drops);
+            printf("%d is the maximum safe floor, found after %d drops.\n", half - 1, *drops);
             return(0);
         }
-        egg_drop_from_floor(eggs[*egg_count - 1], half);
-        ++drops;
+        if(*egg_count < 2)
+        {
+            printf("NUMBERS AT EXIT: %d %d\n", top, bottom + 1);
+            return(bottom);
+        }
+        egg_drop_from_floor(e, half);
+        ++(*drops);
 
-        if(egg_is_broken(eggs[*egg_count - 1]) != 0)
+        if(egg_is_broken(e) != 0)
         {
             printf("#floor %d CRACK\n", half);
             top = half;
             half = half - ((top - bottom) / 2);
-            destroy_egg(eggs[*egg_count - 1]);
+            destroy_egg(e);
+            e = lay_egg();
             --(*egg_count);
         }
         else
@@ -46,16 +66,21 @@ int findFloor(egg **eggs, int *egg_count, int floors)
             bottom = half;
             half = half + ((top - bottom) / 2);
         }
+        
     }
-    return(1);
+    puts("Leaving find floor");
+    return(bottom);
 }
 
 
 int main(int argc, char **argv)
 {
-    int floors = 0;
+    long int floors = 0;
     int egg_count = 0;
+    int bottom = 0;
+    int drops = 0;
 
+    //Checking for args.
     if(argc <= 1)
     {
         puts("I need at least one egg, and a number of floors.");
@@ -64,28 +89,29 @@ int main(int argc, char **argv)
 
     egg_count = strtol(argv[2], NULL, 10);
     floors = strtol(argv[1], NULL, 10);
-    printf("eggs: %d\nfloors: %d\n", egg_count, floors);
-    egg **eggs = calloc(sizeof(struct _egg*), egg_count);
-    
-    //Fill egg array before testing.
-    for(int i = 0; i < egg_count; ++i)
+    printf("eggs: %d\nfloors: %ld\n", egg_count, floors);
+
+
+    //Checking for invalid egg numbers or over/underflows.
+    if(egg_count < 1)
     {
-        eggs[i] = lay_egg();
+        puts("I need at least one egg...");
+        return(1);
     }
 
+    //Make the first egg. MIGHT REMOVE.
+    egg *e = calloc(sizeof(struct _egg*), 1);
+    e= lay_egg();
 
-    int bruteFlag = findFloor(eggs, &egg_count, floors);
+    bottom = findFloor(e, &egg_count, floors, &drops);
 
-    if(bruteFlag)
+    if(bottom)
     {
         //Brute force time...
-        printf("result: %d\n", bruteUp(eggs[1]) );
+        printf("result: %d\n", bruteUp(e, bottom, &drops) );
     }
-    int deleteIndex = 0;
-    for(; deleteIndex < egg_count; ++deleteIndex)
-    {
-        printf("%d\n", deleteIndex);
-        destroy_egg(eggs[deleteIndex]);
-    }
-    free(eggs);
+
+    //Removing remaining eggs.
+    destroy_egg(e);
+
 }
