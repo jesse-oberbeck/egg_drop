eb3563aa37895633178ca38b061a6c46318f5605
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2017-01-20 21:45:28 -0500

    Nearly forgot to add the actual refactor files.

---
+++ drop.h
@@ -0,0 +1,7 @@
+void checkAndPrint(egg *e, int floor);
+
+int bruteUp(int bottom, int *drops, int floors);
+
+int findFloor(int *egg_count, long int floors, int *drops, int *top);
+
+int twoEggs(int *floors, int *bottom, int *drops, int *egg_count);
---
+++ test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "egg.h"
+#include "drop.h"
+
+int main(int argc, char **argv)
+{
+    long int floors = 0;
+    int egg_count = 0;
+    //Bottom moves up as the program goes through functions to reduce
+    //the number of guesses for each approach.
+    int bottom = 1;
+    //Top changes with the eggs over 2 approach, but it seems it neither
+    //hurts nor helps the outcome.
+    int top = 0;
+    int drops = 0;
+
+    //Checking for args.
+    if(argc <= 1)
+    {
+        puts("I need at least one egg, and a number of floors.");
+        return(1);
+    }
+
+    egg_count = strtol(argv[2], NULL, 10);
+    floors = strtol(argv[1], NULL, 10);
+    printf("eggs: %d\nfloors: %ld\n\n", egg_count, floors);
+
+
+    //Checking for invalid egg numbers or over/underflows.
+    if(egg_count < 1)
+    {
+        puts("I need at least one egg...");
+        return(1);
+    }
+
+    if(egg_count > 2)
+    {
+        bottom = findFloor(&egg_count, floors, &drops, &top);
+    }
+
+    if(bottom)
+    {
+        int remainingFloors = top - bottom;
+        bottom = twoEggs(&remainingFloors, &bottom, &drops, &egg_count);
+    }
+
+    if(bottom)
+    {
+        //Brute force time...
+        printf("result: %d\n", bruteUp(bottom, &drops, floors) );
+    }
+}
